d58c0d6ebbe512e9864da417ddbbb76a2aada430
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2026-02-22 22:38:07 +0200

    remove leading / trailing white spaces from username and password and also converts username to lower-case for better login user experience

--- backend/src/main/java/com/church/t1/dto/request/AuthRequest.java
+++ backend/src/main/java/com/church/t1/dto/request/AuthRequest.java
@@ -3,4 +3,9 @@ package com.church.t1.dto.request;
 public record AuthRequest(
         String username,
         String password
-) {}
+) {
+    public AuthRequest {
+        username = username != null ? username.strip().toLowerCase() : null;
+        password = password != null ? password.strip() : null;
+    }
+}
